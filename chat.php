c70b2ff19c579e135515e3aaafc90b960d39aff0
jeykumar3896 <jeykumar3896@gmail.com>
2018-06-29 17:17:08 +0530

    Updation

---
+++ chat.php
@@ -0,0 +1,32 @@
+<?php
+// session_start();
+// isset(!$_SESSION['user'])
+// 	header("locaion:login.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Chat Page</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="box-left">
+				
+			</div>
+			<div class="box-center">
+				<div class="chat-box w-100">
+					
+				</div>
+			</div>
+			<div class="box-right">
+				
+			</div>
+			<div class="clear"></div>
+		</div>
+
+	</div>
+	<script type="text/javascript" src="scripts/app.js"></script>
+</body>
+</html>
--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,6 @@
-// function ajax( obj ){
-// 	var xhttp, responseData;
-// 	if (window.XMLHttpRequest) {
-// 	    xhttp = new XMLHttpRequest();
-// 	} else {
-// 	    // code for IE6, IE5
-// 	    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-// 	}
+// var innerHeight = window.innerHeight;
 
-// 	xhttp.onprogress = function(){
-// 		obj.loading();
-// 	};
+(function(){
+	document.querySelector('.box-left').style.height = window.innerHeight;
+})();
 
-// 	xhttp.onreadystatechange = function() {
-// 		if (xhttp.readyState == 4 && xhttp.status == 200) {
-// 			obj.success();
-// 		}else{
-// 			obj.error();
-// 		}
-// 	};
-
-// 	xhttp.open(obj.method, obj.url, true);
-// 	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-// 	xhttp.send(JSON.stringify(obj.data));
-
-// }
